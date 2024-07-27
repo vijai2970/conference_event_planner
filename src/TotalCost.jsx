4eae8767d29d4823b8cc7cd33b25216c7865a039
VS <vijai2970@gmail.com>
2024-07-27 12:48:28 -0400

    final commit

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -3,7 +3,7 @@ import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
   
-
+const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
   return (
     <div className="pricing-app">
       <div className="display_box">
@@ -12,11 +12,11 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
+            ${total_amount}
+        </h2>
          
-            <div>
-             
+            <div className="render_items">
+                <ItemsDisplay />
             </div>
         </div>
       </div>
